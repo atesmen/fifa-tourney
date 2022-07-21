5cbfe1ecc6b534a0b562d865343f89805a2206a1
atesmen <63913174+atesmen@users.noreply.github.com>
2022-07-20 17:08:58 -0700

    done

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function Match() {
-  return <div></div>;
+export default function Match(props) {
+  console.log(props.match)
+  return <article class="Match">
+          <h1>{props.playerOneName} <span>vs</span> {props.playerTwoName}</h1>
+          <h2>Player {props.winnerName} is the winner by {props.match.scoreDifference}!</h2>
+          <h2>player {props.winnerName} is the winner</h2>
+        </article>
+  ;
 }
--- src/components/MatchesList.js
+++ src/components/MatchesList.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import matches from '../data/matchesData';
+import Match  from './Match';
+import players from '../data/playersData';
 
 export default function MatchesList() {
-  return <div></div>;
+  const matchArray = matches.map((match) =>{
+    console.log(match)
+   const winnerId = match.winner 
+   let winnerName = "" 
+   if(winnerId !== 0){
+      winnerName = players[`id${winnerId}`].name
+    }
+    const playerOneId = match.players[0]
+    const playerTwoId = match.players[1] 
+    const playerOneName = players[`id${playerOneId}`].name
+    const playerTwoName = players[`id${playerTwoId}`].name
+    return <Match match ={match} winnerName ={winnerName} playerOneName ={playerOneName} playerTwoName ={playerTwoName}/>
+  })
+  return <div>
+    {matchArray}
+  </div>;
 }
